a57034f9cb7c40dc6d5d7f5dbb7e71840af76dd5
IamSteveAde <112140086+IamSteveAde@users.noreply.github.com>
2023-08-30 04:06:06 -0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -142,3 +142,61 @@ setTimeout(() => {
     loader.style.display = 'none'; // Hide the loader
     document.body.style.overflow = 'auto'; // Enable scrolling
 }, 8000); // Adjust the delay as needed
+const reviewSlides = document.querySelectorAll('.review-slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    reviewSlides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    currentSlide++;
+    if (currentSlide >= reviewSlides.length) {
+        currentSlide = 0;
+    }
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide--;
+    if (currentSlide < 0) {
+        currentSlide = reviewSlides.length - 1;
+    }
+    showSlide(currentSlide);
+}
+
+document.getElementById('nextBtn').addEventListener('click', nextSlide);
+document.getElementById('prevBtn').addEventListener('click', prevSlide);
+
+// Show the initial slide
+showSlide(currentSlide);
+const firstName = document.getElementById('firstname_inputfield');
+const lastname = document.getElementById('lastname_inputfield');
+const email = document.getElementById('email_inputfield');
+const tel = document.getElementById('tel_inputfield');
+const subject = document.getElementById('subject_inputfield');
+const message = document.getElementById('message_textarea');
+const contact_us_form = document.getElementById('homeContactForm');
+const correct_response_indicator = document.getElementById('correct_response');
+const error_response_indicator = document.getElementById('error_response');
+
+const SubmitContactForm = (e) =>{
+    e.preventDefault();
+    error_response_indicator.style.display="none";
+    correct_response_indicator.style.display="none";
+    axios.defaults.headers.post['Content-Type'] = 'application/json';
+    axios.post('https://formsubmit.co/ajax/adediranstephen2000@gmail.com.com', {
+    _subject: "Contact Form Questions/Inquiries",
+    FirstName: firstName.value,
+    LastName: lastname.value,
+    Email: email.value,
+    Phone: tel.value,
+    Subject: subject.value,
+    Message: message.value
+    })
+    .then(response => response.status == 200 ? (correct_response_indicator.style.display="block", error_response_indicator.style.display="none"): null)
+    .catch(error => (console.log(error), correct_response_indicator.style.display="none", error_response_indicator.style.display="block"));
+    contact_us_form.reset();
+}
